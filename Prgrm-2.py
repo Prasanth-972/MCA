29ccc8a06946fb4cd97e4c788a3a1d4c5157206e
Prasanth Nair <149087444+Prasanth-972@users.noreply.github.com>
2024-12-04 20:49:23 +0530

    Add files via upload

---
+++ Prgrm-2.py
@@ -0,0 +1,24 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+if num1 >= num2 and num1 >= num3:
+    n = num1
+elif num2 >= num1 and num2 >= num3:
+    n = num2
+else:
+    n = num3
+    print(f"The biggest number is: {n}")
+
+nn = n**2 
+nnn = n**3 
+
+result = n + nn + nnn
+print(f"n + nn + nnn = {n} + {nn} + {nnn} = {result}")
+
+radius = n
+
+area = 3.14 * radius ** 2
+perimeter = 2 * 3.14 * radius
+print(f"\nFor a circle with radius {radius}:")
+print(f"Area = {area:.2f}")
+print(f"Perimeter (Circumference) = {perimeter:.2f}")
